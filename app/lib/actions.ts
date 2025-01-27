d3c2c6021d02ab51179afbff41ca0a581317c1ba
vostavhy <vostavhy@gmail.com>
2025-01-27 21:10:36 +0100

    feat: implement error notifications for create-form fields

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,23 +5,46 @@ import { client } from '../seed/route';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+export type State = {
+  message?: string | null;
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+};
+
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer',
+  }),
+  amount: z.coerce.number().gt(0, { message: 'Please enter amount greater than 0' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  const { status, customerId, amount } = validatedFields.data;
 
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
@@ -53,14 +76,16 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await client.query(
-    `
+  try {
+    await client.query(
+      `
     UPDATE invoices
     SET customer_id = $1, amount = $2, status = $3
     WHERE id = $4
   `,
-    [customerId, amountInCents, status, id]
-  );
+      [customerId, amountInCents, status, id]
+    );
+  } catch (error) {}
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
